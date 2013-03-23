76345ddabf170b5295303bc7d1d932ea7e1d64be
Peter A. Cejchan <tyapca7@gmail.com>
2013-03-23 07:38:45 +0100

    added NormPostNoPriorNext() and NormPostInfPriorNext() to bayes

--- bayes/_tst/normpostsim_test.go
+++ bayes/_tst/normpostsim_test.go
@@ -7,6 +7,7 @@ import (
 
 // Test against R:LearningBayes
 func TestNormPostSimNoPrior(t *testing.T) {
+	fmt.Println("Testing NormPostSimNoPrior()")
 	m := 100000
 	d := []float64{-67, -48, 6, 8, 14, 16, 23, 24, 28, 29, 41, 49, 67, 60, 75}
 	means, vars := NormPostSimNoPrior(d, m)
@@ -24,6 +25,36 @@ func TestNormPostSimNoPrior(t *testing.T) {
 
 	if abs(x/y-1) > 1e4 {
 		fmt.Println("vars failed: x, y ", x, y)
+		t.Error()
+	}
+}
+
+func TestNormPostNoPriorNext(t *testing.T) {
+	fmt.Println("Testing NormPostNoPriorNext()")
+	d := []float64{-67, -48, 6, 8, 14, 16, 23, 24, 28, 29, 41, 49, 67, 60, 75}
+	m := 100000
+	mn := 0.0
+	vr := 0.0
+
+	for i := 0; i < m; i++ {
+		mni, vri := NormPostNoPriorNext(d)
+		mn += mni
+		vr += vri
+	}
+	mn /= float64(m)
+	vr /= float64(m)
+
+	y := 21.67034
+
+	if abs(mn/y-1) > 1e4 {
+		fmt.Println("means failed: mn, y ", mn, y)
+		t.Error()
+	}
+
+	y = 1735.703
+
+	if abs(vr/y-1) > 1e4 {
+		fmt.Println("vars failed: vr, y ", vr, y)
 		t.Error()
 	}
 }
